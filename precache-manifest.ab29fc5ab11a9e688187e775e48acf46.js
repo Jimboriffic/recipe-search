d64a6b7e9c3dad4246b6e08c8e5b0bc0f1fc83b9
James <james.sliwa@gmail.com>
2020-04-11 18:46:18 +0200

    Updates

---
+++ precache-manifest.ab29fc5ab11a9e688187e775e48acf46.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c954033f8c5df1185f17de64bf8bd540",
+    "url": "/recipe-search/index.html"
+  },
+  {
+    "revision": "5fbc404dfbc0ed06b058",
+    "url": "/recipe-search/static/css/main.e2ed637c.chunk.css"
+  },
+  {
+    "revision": "59a6deaf9a57c028813e",
+    "url": "/recipe-search/static/js/2.7dcd748a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/recipe-search/static/js/2.7dcd748a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5fbc404dfbc0ed06b058",
+    "url": "/recipe-search/static/js/main.248624b5.chunk.js"
+  },
+  {
+    "revision": "80c9a24404c55ad3b064",
+    "url": "/recipe-search/static/js/runtime-main.ccb32165.js"
+  }
+]);
